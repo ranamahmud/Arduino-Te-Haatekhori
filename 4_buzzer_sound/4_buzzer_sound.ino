f6532bec5c7c80ba9f3d85fc385638f7234e265b
Md Rana Mahmud <ranamd0@gmail.com>
2021-07-30 12:46:58 +0600

    buzzer sound done

---
+++ 4_buzzer_sound/4_buzzer_sound.ino
@@ -0,0 +1,8 @@
+int buzzerPin = 12;
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(buzzerPin, HIGH);
+}
---
+++ 4_buzzer_sound_2/4_buzzer_sound_2.ino
@@ -0,0 +1,11 @@
+int buzzerPin = 12;
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
+  digitalWrite(buzzerPin, LOW);
+  delay(1000);
+}
